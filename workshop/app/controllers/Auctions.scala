43bfb26e90b1ffd02a96c8f4514e3533a93985d4
Pascal Voitot <pascal.voitot.dev@gmail.com>
2013-10-20 17:27:25 +0200

    added CustomReactiveMongoAutoSourceto enhance insert actions + added default q={} to get all auctions by restangular

--- workshop/app/controllers/Auctions.scala
+++ workshop/app/controllers/Auctions.scala
@@ -13,15 +13,33 @@ import play.api.libs.functional.syntax._
 import play.modules.reactivemongo.json.collection.JSONCollection
 import play.modules.reactivemongo.json.BSONFormats._
 import play.autosource.reactivemongo._
-import play.autosource.core.AfterAction
 
 import reactivemongo.bson.BSONObjectID
 
 import org.mandubian.actorroom._
 import models._
 
+
+class CustomReactiveMongoAutoSource(coll: JSONCollection) extends ReactiveMongoAutoSource[Auction](coll: JSONCollection) {
+  override def insert(auction: Auction)(implicit ctx: scala.concurrent.ExecutionContext): Future[BSONObjectID] = super.insert(auction) map { id =>
+    services.AuctionGlobal.room.foreach{ room =>
+      room.supervisor ! Broadcast(
+        auction.seller,
+        Json.obj(
+          "kind" -> "auction", 
+          "action" -> "new",
+          "value" -> (Json.obj("id" -> id.stringify) ++ Json.toJson(auction).as[JsObject])
+        )
+      )
+    }
+    id
+  }
+}
+
 object Auctions extends ReactiveMongoAutoSourceController[Auction] {
   lazy val coll = storage.auctions
+
+  override lazy val res = new CustomReactiveMongoAutoSource(coll)
 
   def addOffer(idAuction: String) = Action.async(parse.json) { implicit request =>
     request.body.validate[Offer].fold(
@@ -30,41 +48,25 @@ object Auctions extends ReactiveMongoAutoSourceController[Auction] {
         case Some((auction, id)) => {
           val updatedAuction = auction.copy( offers = auction.offers:+(offer) )
 
-          services.AuctionGlobal.room.foreach{
-            _.supervisor ! Broadcast(
-              offer.buyer,
-              Json.obj(
-                "id" -> Json.toJson(id), 
-                "kind" -> "offer", 
-                "action" -> "new",
-                "value" -> (Json.obj("id" -> idAuction) ++ Json.toJson(updatedAuction).as[JsObject])
+          this.res.update(id, updatedAuction).map { _ =>
+            services.AuctionGlobal.room.foreach{ room =>
+              room.supervisor ! Broadcast(
+                offer.buyer,
+                Json.obj(
+                  "id" -> Json.toJson(id), 
+                  "kind" -> "offer", 
+                  "action" -> "new",
+                  "value" -> (Json.obj("id" -> idAuction) ++ Json.toJson(updatedAuction).as[JsObject])
+                )
               )
-            )
-          }
+            }
 
-          this.res.update(id, updatedAuction).map( unit => Ok(Json.toJson(updatedAuction)) )
+            Ok(Json.toJson(updatedAuction)) 
+          }
         }
         case None => Future(NotFound)
       }
     )
-  }
-
-  override def insert = Action.async(parse.json){ request =>
-    Json.fromJson[Auction](request.body).map{ auction =>
-      res.insert(auction).map{ id =>
-        services.AuctionGlobal.room.foreach{
-          _.supervisor ! Broadcast(
-            auction.seller,
-            Json.obj(
-              "kind" -> "auction", 
-              "action" -> "new",
-              "value" -> (Json.obj("id" -> id.stringify) ++ Json.toJson(auction).as[JsObject])
-            )
-          )
-        }
-        Ok(Json.toJson(id))
-      }
-    }.recoverTotal{ e => Future.successful(BadRequest(JsError.toFlatJson(e))) }
   }
 
 }
--- workshop/public/javascripts/auctions/auctionsService.js
+++ workshop/public/javascripts/auctions/auctionsService.js
@@ -3,7 +3,7 @@ app.factory('Auctions', ['Restangular', function (Restangular) {
   // Will do a GET at '/Auctions' and return a promise
   // which will return a JSON array when resolved
   function all() {
-    return Restangular.all('auctions').getList();
+    return Restangular.all('auctions').getList({ "q" : "{}" });
   }
 
   // Will do a GET at '/Auctions/:id' and return a promise
